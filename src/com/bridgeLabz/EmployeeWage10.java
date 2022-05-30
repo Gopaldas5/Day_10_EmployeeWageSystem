9f0c1ac0d61b20747d33ebdd62dc931eaf8b63c0
GopalDas <gopalkrdas1997@gmail.com>
2022-05-31 00:55:47 +0530

    ADD: Checking the attendance of the employees

--- src/com/bridgeLabz/EmployeeWage10.java
+++ src/com/bridgeLabz/EmployeeWage10.java
@@ -1,7 +1,18 @@
 package com.bridgeLabz;
 
+import java.util.Random;
+
 public class EmployeeWage10 {
     public static void main(String[] args) {
         System.out.println("Welcome to the employee wage computation on master branch");
+        int IS_ABSENT_TODAY = 0;
+        Random random = new Random();
+        int employeeAttendanceCheck = random.nextInt(2);
+
+        if (employeeAttendanceCheck  == IS_ABSENT_TODAY) {
+            System.out.println("Employee is absent today");
+        } else {
+            System.out.println("Employee is Present today");
+        }
     }
 }
